5dae86f11536a0a749c67ef1049b58dfa613d22a
SUZ-tsinghua <suz22@mails.tsinghua.edu.cn>
2024-12-25 21:09:56 -0800

    update example.sh

--- examples/baselines/ppo/training_examples.sh
+++ examples/baselines/ppo/training_examples.sh
@@ -11,7 +11,7 @@ for seed in 0 42 3407
 do
     CUDA_VISIBLE_DEVICES=0 python ppo.py --env_id=RollBall-v1 --num_envs=512 --update_epochs=8 --num_minibatches=32 --total_timesteps=30_000_000 --num-steps=80 --num_eval_steps=80 --gamma=0.95 --wandb_entity=boyuanchen21-tsinghua-university --track --seed=${seed}
     CUDA_VISIBLE_DEVICES=0 python ppo_adapt.py --env_id=RollBall-v1 --num_envs=512 --update_epochs=8 --num_minibatches=32 --total_timesteps=30_000_000 --num-steps=80 --num_eval_steps=80 --gamma=0.95 --wandb_entity=boyuanchen21-tsinghua-university --track --seed=${seed}
-    CUDA_VISIBLE_DEVICES=0 python ppo_rgb.py --env_id=RollBall-v1 --num_envs=512 --update_epochs=8 --num_minibatches=32 --total_timesteps=40_000_000 --num-steps=80 --num_eval_steps=80 --gamma=0.95 --wandb_entity=boyuanchen21-tsinghua-university --track --seed=${seed}
+    CUDA_VISIBLE_DEVICES=0 python ppo_rgb.py --env_id=RollBall-v1 --num_envs=512 --update_epochs=8 --num_minibatches=32 --total_timesteps=30_000_000 --num-steps=80 --num_eval_steps=80 --gamma=0.95 --wandb_entity=boyuanchen21-tsinghua-university --track --seed=${seed}
 done
 
 # PickFromCabinetDrawer-v1
